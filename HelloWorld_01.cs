e477648e0c8f7d3253c58cc56e6c05b773aed412
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-07-15 19:13:04 +0900

    Create HelloWorld_01.cs

---
+++ HelloWorld_01.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//C# 기본 문법 시작
+namespace HelloCsharpWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write는 줄바꿈을 하지 않는다
+            Console.WriteLine("C# hello world"); 
+            Console.WriteLine("C#에 온 것을 환영합니다.");
+            Console.WriteLine("{0} {1}", args[1], args.Length-1);
+        }
+    }
+}
+ 
