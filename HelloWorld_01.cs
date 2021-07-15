15bb57bff41e645e56b4cefd22593ccbe26aecd5
RASCALCS <63025037+kashmir1cs@users.noreply.github.com>
2021-07-15 19:18:02 +0900

    C# First Practice

--- HelloWorld_01.cs
+++ HelloWorld_01.cs
@@ -14,7 +14,9 @@ namespace HelloCsharpWorld
             //write는 줄바꿈을 하지 않는다
             Console.WriteLine("C# hello world"); 
             Console.WriteLine("C#에 온 것을 환영합니다.");
-            Console.WriteLine("{0} {1}", args[1], args.Length-1);
+            Console.WriteLine("{0} {1}", args[1], args.Length-1); //Python과 유사하게 문자열 리스트 인덱싱 가능
+            //CMD cursor에서 "HelloCsharpWorld text1 text2 text3 입력할 경우
+            //출력 결과 : text2 2 (3-1=2)
         }
     }
 }
